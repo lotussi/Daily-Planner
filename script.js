14aa753ddce94c1c9048016d65264b841248adbe
noori <rajilabana@gmail.com>
2023-01-21 09:05:01 +0000

    add starter code to the repository

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".saveBtn").on("click", function(){
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+
+    })
+})
