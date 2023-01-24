2ec007fe6e7dfedeecee2bb1f576c349e5df25d1
noori <rajilabana@gmail.com>
2023-01-24 09:27:45 +0000

    update readme and add screenshots to readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Daily-Planner
+## Discription
+ This code is built to make daily planner, which helps developers to follow their day plans. This code is built with help of jquery using moment js for display current date and month.
+
+
+ # Usage
+ ![Alt text](Screenshot_20230123_141814.png)
+ ![Alt text](Screenshot_20230123_143525.png)
+
+ ## License
+ MIT License
+
+
+This week, MIT License
+Copyright(c) [2022] [your name]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+you'll build a portfolio page, which you can add to as the course progresses. 
+
+
+ 
+## Badges
+N/A
--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-$(document).ready(function () {
-    $(".saveBtn").on("click", function () {
-        var value = $(this).siblings(".description").val();
+
+$(document).ready(function() {
+    $(".saveBtn").on("click", function(){
+      var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, value);
-        console.log(value);
+        console.log (value);
         console.log(time);
-    })
+   })
 })
 // retrieving value from local storage and puting the value in each corosponding time block
 $("#hour-9 .description").val(localStorage.getItem("hour-9"))
-$("#hour-10 .description").val(localStorage.getItem("hour-9"))
-$("#hour-11 .description").val(localStorage.getItem("hour-9"))
-$("#hour-12 .description").val(localStorage.getItem("hour-9"))
-$("#hour-13 .description").val(localStorage.getItem("hour-9"))
-$("#hour-14 .description").val(localStorage.getItem("hour-9"))
-$("#hour-15 .description").val(localStorage.getItem("hour-9"))
-$("#hour-16 .description").val(localStorage.getItem("hour-9"))
-$("#hour-17 .description").val(localStorage.getItem("hour-9"))
 
-//Getting the current day and displaying on page
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
+
+// Getting the current time and displaying on page
 var currentDay = document.getElementById("currentDay");
 currentDay.innerHTML = moment().format('dddd, MMMM, Do');
+// looping through each time block and comparing timebloc against current hour and assiging the css class to time blocks
+$(".time-block").each(function(){
+    var presentHour = moment().hour()
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1])
+    if(timeBlockHour < presentHour){
+        $(this).addClass("past")
+    }else if(timeBlockHour === presentHour){
+        $(this).addClass("present")
+    }else if(timeBlockHour > presentHour){
+        $(this).addClass("future")
+    }
+})
